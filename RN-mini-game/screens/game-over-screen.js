ed86e2f144d27cbff2fdd80ca89cdb7eea723839
gyojinnK <ksmug1998@gmail.com>
2024-08-22 17:21:33 +0900

    feat: 전경 이미지 추가

--- RN-mini-game/screens/game-over-screen.js
+++ RN-mini-game/screens/game-over-screen.js
@@ -1,8 +1,40 @@
 import React from "react";
-import { Text } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Colors from "../constants/colors";
+import Title from "../components/ui/title";
 
 const GameOverScreen = () => {
-  return <Text>Game Over!</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title innerText={"GAME OVER!"} />
+      <View style={styles.imageContainer}>
+        <Image source={require("../assets/success.png")} style={styles.image} />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y.</Text>
+    </View>
+  );
 };
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
